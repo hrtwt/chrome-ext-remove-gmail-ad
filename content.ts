5ce18a65d6880dd863d9a5badee1426f1d978306
hrtwt <29752757+hrtwt@users.noreply.github.com>
2024-10-13 13:25:36 +0900

    impl feat

--- content.ts
+++ content.ts
@@ -1,1 +1,7 @@
-console.log("loaded content.js");
+new MutationObserver(() => {
+  document.querySelectorAll(
+    'tr:has(> td[data-tooltip="選択"][style*="visibility: hidden"])',
+  ).forEach((e) => {
+    e.remove();
+  });
+}).observe(document.body, { childList: true, subtree: true });
